abf28d0506ea1bf2df7c86ecdc57e11ddab1516c
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-01-28 16:32:34 +0100

    add logging

--- scripts/cd4pe.py
+++ scripts/cd4pe.py
@@ -32,6 +32,13 @@ LOGGER.setLevel(logging.INFO)
 def print_json(content):
     print(json.dumps(content, indent=2))
 
+def add2log(content):
+    print(content)
+    if data['log'] == "":
+        data['log'] = content
+    else:
+        data['log'] = data['log'] + "\n" + content
+
 def connect_cd4pe(endpoint, username, password):
     global CD4PE_CLIENT
     CD4PE_CLIENT = CD4PE(endpoint, True)
@@ -44,7 +51,7 @@ def search_latest_pipeline(repo_name, gitCommitId):
             result = {}
             result['id'] = response['rows'][x]['pipelineId']
             result['eventId'] = response['rows'][x]['id']
-            print( "Pipeline #: " + str(result['eventId']))
+            add2log( "Pipeline #: " + str(result['eventId']))
             response = None
             return result
 
@@ -55,35 +62,34 @@ def switcher_jobstatus(job_status):
     }
     return switcher.get(job_status, "unknown status: " + str(job_status))
 
-def report_pipeline_stages(repo_name, pipeline_id, pipeline_stage):
-    response = CD4PE_CLIENT.get_pipeline(repo_name=repo_name, pipeline_id=pipeline_id).json()
-    no_of_stages = len(response['stages'])
-    print("Number of stages in pipeline: " + str(no_of_stages))
+def report_pipeline_stages(pipeline_json, pipeline_stage):
+    no_of_stages = len(pipeline_json['stages'])
+    add2log("Number of stages in pipeline: " + str(no_of_stages))
     for x in range(no_of_stages):
         stage_failure = False
-        stage = response['stages'][x]
+        stage = pipeline_json['stages'][x]
         if stage['stageName'] == pipeline_stage:
-            print("Stage " + str(stage['stageNum']) + ": " + stage['stageName'] )
+            add2log("Stage " + str(stage['stageNum']) + ": " + stage['stageName'] )
             data['build']['queue_id'] = stage['stageNum']
             no_of_stage_jobs = len(stage['destinations'])
-            print("  Number of jobs in stage: " + str(no_of_stage_jobs))
+            add2log("  Number of jobs in stage: " + str(no_of_stage_jobs))
             for y in range(no_of_stage_jobs):
                 stage_job = stage['destinations'][y]
                 if 'vmJobEvent' in stage_job:
-                    print("  Job name: " + stage_job['vmJobEvent']['jobName'])
-                    print("  Job status: " + switcher_jobstatus(stage_job['vmJobEvent']['jobStatus']))
+                    add2log("  Job name: " + stage_job['vmJobEvent']['jobName'])
+                    add2log("  Job status: " + switcher_jobstatus(stage_job['vmJobEvent']['jobStatus']))
                     data['build']['timestamp'] = stage_job['vmJobEvent']['eventTime']
                     if switcher_jobstatus(stage_job['vmJobEvent']['jobStatus']) != 'Success':
                         stage_failure = True
                 elif 'deploymentAppEvent' in stage_job:
-                    print("  Deployment: " + stage_job['deploymentAppEvent']['deploymentPlanName'] + " to " + stage_job['deploymentAppEvent']['targetBranch'])
-                    print("  Deployment status: " + stage_job['deploymentAppEvent']['deploymentState'] )
+                    add2log("  Deployment: " + stage_job['deploymentAppEvent']['deploymentPlanName'] + " to " + stage_job['deploymentAppEvent']['targetBranch'])
+                    add2log("  Deployment status: " + stage_job['deploymentAppEvent']['deploymentState'] )
                     data['build']['timestamp'] = stage_job['deploymentAppEvent']['eventTime']
                     if stage_job['deploymentAppEvent']['deploymentState'] != 'DONE':
                         stage_failure = True
                 elif 'peImpactAnalysisEvent' in stage_job:
-                    print("  Impact Analysis: on " + str(len(stage_job['peImpactAnalysisEvent']['environments'])) + " environments")
-                    print("  Impact Analysis status: " + stage_job['peImpactAnalysisEvent']['state'])
+                    add2log("  Impact Analysis: on " + str(len(stage_job['peImpactAnalysisEvent']['environments'])) + " environments")
+                    add2log("  Impact Analysis status: " + stage_job['peImpactAnalysisEvent']['state'])
                     data['build']['timestamp'] = stage_job['peImpactAnalysisEvent']['eventTime']
                     if stage_job['peImpactAnalysisEvent']['state'] != 'DONE':
                         stage_failure = True
@@ -93,15 +99,13 @@ def report_pipeline_stages(repo_name, pipeline_id, pipeline_stage):
             else:
                 result = "Stage succeeded"
                 data['build']['status'] = 'SUCCESS'
-            print(result)
-            response = None
+            add2log(result)
             return result
 
-def get_IA_from_pipeline(repo_name, pipeline_id):
-    response = CD4PE_CLIENT.get_pipeline(repo_name=repo_name, pipeline_id=pipeline_id).json()
-    no_of_stages = len(response['stages'])
+def get_IA_from_pipeline(pipeline_json):
+    no_of_stages = len(pipeline_json['stages'])
     for x in range(no_of_stages):
-        stage = response['stages'][x]
+        stage = pipeline_json['stages'][x]
         no_of_stage_jobs = len(stage['destinations'])
         for y in range(no_of_stage_jobs):
             stage_job = stage['destinations'][y]
@@ -117,33 +121,32 @@ def get_impact_analysis_node_report(impact_analysis_id):
     no_of_environments = len(response['results'])
     for x in range(no_of_environments):
         env_result = response['results'][x]
-        print("Impact Analysis report on environment: " + env_result['environment'])
+        add2log("Impact Analysis report on environment: " + env_result['environment'])
         env_result_id = env_result['environmentResultId']
         env_result = CD4PE_CLIENT.search_impacted_nodes(environment_result_id=env_result_id).json()
         no_of_impacted_nodes = len(env_result['rows'])
-        print( "Number of impacted nodes: " + str(no_of_impacted_nodes))
+        add2log( "Number of impacted nodes: " + str(no_of_impacted_nodes))
         safe_report = True
         for y in range(no_of_impacted_nodes):
             node_result = env_result['rows'][y]
             if 'compileFailed' in node_result:
-                print("  Node " + node_result['certnameLowercase'] + ": Failed compilation")
+                add2log("  Node " + node_result['certnameLowercase'] + ": Failed compilation")
                 safe_report = False
             else:
-                print("  Node " + node_result['certnameLowercase'] + " resources: " +
+                add2log("  Node " + node_result['certnameLowercase'] + " resources: " +
                     str(len(node_result['resourcesAdded'])) + " added, " +
                     str(len(node_result['resourcesModified'])) + " modified, " +
                     str(len(node_result['resourcesRemoved'])) + " removed.")
         result = "Impact Analysis: safe" if (safe_report == True) else "Impact Analysis: unsafe"
-        print(result)
+        add2log(result)
         response = None
         return result
 
-def get_pending_approvals(repo_name, pipeline_id):
-    response = CD4PE_CLIENT.get_pipeline(repo_name=repo_name, pipeline_id=pipeline_id).json()
-    no_of_stages = len(response['stages'])
+def get_pending_approvals(pipeline_json):
+    no_of_stages = len(pipeline_json['stages'])
     table = {}
     for x in range(no_of_stages):
-        stage = response['stages'][x]
+        stage = pipeline_json['stages'][x]
         no_of_stage_jobs = len(stage['destinations'])
         for y in range(no_of_stage_jobs):
             stage_job = stage['destinations'][y]
@@ -158,34 +161,35 @@ def approve_deployment(deployment_id):
     return response
 
 # Start of code execution
-connect_cd4pe(endpoint=endpoint, username=user, password=pwd)
-pipeline = search_latest_pipeline(repo_name=repo, gitCommitId=commitSha)
-buildinfo = CD4PE_CLIENT.get_pipeline(repo_name=repo, pipeline_id=pipeline['id']).json()
-
 data = {}
 data['name'] = 'cd4pe-pipeline'
 data['display_name'] = 'cd4pe-pipeline'
 data['build'] = {}
+data['log'] = ""
+
+connect_cd4pe(endpoint=endpoint, username=user, password=pwd)
+pipeline = search_latest_pipeline(repo_name=repo, gitCommitId=commitSha)
+pipeline_json = CD4PE_CLIENT.get_pipeline(repo_name=repo, pipeline_id=pipeline['id']).json()
+
 data['url'] = "main/repositories/" + repo + "?pipelineId=" + str(pipeline['id'])
 data['build']['full_url'] = endpoint + "/" + CD4PE_CLIENT.api_ajax.owner + "/repositories/" + repo + "?pipelineId=" + str(pipeline['id']) + "&eventId=" + str(pipeline['eventId'])
 data['build']['number'] = pipeline['eventId']
 data['build']['phase'] = stagename
 data['build']['url'] = "/" + CD4PE_CLIENT.api_ajax.owner + "/repositories/" + repo + "?pipelineId=" + str(pipeline['id']) + "&eventId=" + str(pipeline['eventId'])
 data['scm'] = {}
-data['scm']['url'] = buildinfo['buildStage']['imageEvent']['repoUrl']
-data['scm']['branch'] = buildinfo['buildStage']['imageEvent']['branch']
-data['scm']['commit'] = buildinfo['buildStage']['imageEvent']['commitId']
+data['scm']['url'] = pipeline_json['buildStage']['imageEvent']['repoUrl']
+data['scm']['branch'] = pipeline_json['buildStage']['imageEvent']['branch']
+data['scm']['commit'] = pipeline_json['buildStage']['imageEvent']['commitId']
 data['scm']['changes'] = []
 data['scm']['culprits'] = []
 data['artifacts'] = {}
 
-pipeline_report = report_pipeline_stages(repo_name=repo, pipeline_id=pipeline['id'], pipeline_stage=stagename)
+pipeline_report = report_pipeline_stages(pipeline_json=pipeline_json, pipeline_stage=stagename)
 if stagename == 'Impact Analysis':
-    IA = get_IA_from_pipeline(repo_name=repo, pipeline_id=pipeline['id'])
+    IA = get_IA_from_pipeline(pipeline_json=pipeline_json)
     if IA['Status'] == "DONE":
         report = get_impact_analysis_node_report(impact_analysis_id=IA['Id'])
 
-data['log'] = ""
 data['notes'] = ""
 print_json(data)
 
