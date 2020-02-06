ec3ff4f222ac4d1363f20a9e48bd0d282804ee8c
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-02-06 12:54:04 +0100

    test update - no webhooks

--- scripts/cd4pe.py
+++ scripts/cd4pe.py
@@ -15,6 +15,7 @@ parser       = argparse.ArgumentParser(description='Optional app description')
 parser.add_argument('--commitSha', type=str, help='the Git commit SHA to check')
 parser.add_argument('--maxchanges', type=int, help='how many changes are still deemed safe', default=10)
 parser.add_argument('--stagename', type=str, help='the pipeline stage to report')
+parser.add_argument('--changereq', type=bool, help='(boolean) create a SNOW ticket for deploy approval', , default=False)
 parser.add_argument('--user', type=str, help='the CD4PE username')
 parser.add_argument('--pwd', type=str, help='the CD4PE user password')
 parser.add_argument('--endpoint', type=str, help='the CD4PE endpoint')
@@ -24,6 +25,7 @@ args       = parser.parse_args()
 commitSha  = args.commitSha
 maxchanges = args.maxchanges
 stagename  = args.stagename
+changereq  = args.changereq
 user       = args.user
 pwd        = args.pwd
 endpoint   = args.endpoint
@@ -77,24 +79,52 @@ def report_pipeline_stages(pipeline_json, pipeline_stage):
             add2log("  Number of jobs in stage: " + str(no_of_stage_jobs))
             for y in range(no_of_stage_jobs):
                 stage_job = stage['destinations'][y]
+                eventinfo = {}
                 if 'vmJobEvent' in stage_job:
                     add2log("  Job name: " + stage_job['vmJobEvent']['jobName'])
                     add2log("  Job status: " + switcher_jobstatus(stage_job['vmJobEvent']['jobStatus']))
-                    data['build']['timestamp'] = stage_job['vmJobEvent']['eventTime']
+                    eventinfo['eventName'] = stage_job['vmJobEvent']['jobName']
+                    eventinfo['eventNumber'] = stage_job['vmJobEvent']['vmJobInstanceId']
+                    eventinfo['eventTime'] = stage_job['vmJobEvent']['eventTime']
+                    eventinfo['eventResult'] = switcher_jobstatus(stage_job['vmJobEvent']['jobStatus'])
+                    eventinfo['startTime'] = stage_job['vmJobEvent']['jobStartTime']
+                    eventinfo['endTime'] = stage_job['vmJobEvent']['jobEndTime']
+                    eventinfo['executionTime'] = (eventinfo['endTime'] - eventinfo['startTime'])/1000
+                    data['build']['events'].append(eventinfo) 
                     if switcher_jobstatus(stage_job['vmJobEvent']['jobStatus']) != 'Success':
                         stage_failure = True
                 elif 'deploymentAppEvent' in stage_job:
                     add2log("  Deployment: " + stage_job['deploymentAppEvent']['deploymentPlanName'] + " to " + stage_job['deploymentAppEvent']['targetBranch'])
                     add2log("  Deployment status: " + stage_job['deploymentAppEvent']['deploymentState'] )
-                    data['build']['timestamp'] = stage_job['deploymentAppEvent']['eventTime']
+                    eventinfo['eventName'] = stage_job['deploymentAppEvent']['deploymentPlanName'] + " to " + stage_job['deploymentAppEvent']['targetBranch']
+                    eventinfo['eventNumber'] = stage_job['deploymentAppEvent']['deploymentId']
+                    eventinfo['eventTime'] = stage_job['vmJobEvent']['eventTime']
+                    eventinfo['eventResult'] = stage_job['deploymentAppEvent']['deploymentState']
+                    eventinfo['startTime'] = stage_job['deploymentAppEvent']['deploymentStartTime']
+                    eventinfo['endTime'] = stage_job['deploymentAppEvent']['deploymentEndTime']
+                    eventinfo['executionTime'] = (eventinfo['endTime'] - eventinfo['startTime'])/1000
+                    data['build']['events'].append(eventinfo) 
                     if stage_job['deploymentAppEvent']['deploymentState'] != 'DONE':
                         stage_failure = True
                 elif 'peImpactAnalysisEvent' in stage_job:
                     add2log("  Impact Analysis: on " + str(len(stage_job['peImpactAnalysisEvent']['environments'])) + " environments")
                     add2log("  Impact Analysis status: " + stage_job['peImpactAnalysisEvent']['state'])
-                    data['build']['timestamp'] = stage_job['peImpactAnalysisEvent']['eventTime']
+                    eventinfo['eventName'] = "Impact Analysis on " + str(len(stage_job['peImpactAnalysisEvent']['environments'])) + " environments"
+                    eventinfo['eventNumber'] = stage_job['peImpactAnalysisEvent']['impactAnalysisId']
+                    eventinfo['eventTime'] = stage_job['vmJobEvent']['eventTime']
+                    eventinfo['eventResult'] = stage_job['peImpactAnalysisEvent']['state']
+                    eventinfo['startTime'] = stage_job['peImpactAnalysisEvent']['startTime']
+                    eventinfo['endTime'] = stage_job['peImpactAnalysisEvent']['endTime']
+                    eventinfo['executionTime'] = (eventinfo['endTime'] - eventinfo['startTime'])/1000
+                    data['build']['events'].append(eventinfo) 
                     if stage_job['peImpactAnalysisEvent']['state'] != 'DONE':
                         stage_failure = True
+            first_event = data['build']['events'][0]
+            last_event = data['build']['events'][-1]
+            data['build']['timestamp'] = first_event['eventTime']
+            data['build']['startTime'] = first_event['startTime']
+            data['build']['endTime'] = last_event['endTime']
+            data['build']['executionTime'] = (last_event['endTime'] - first_event['startTime'])/1000
             if stage_failure == True:
                 result = "Stage failed"
                 data['build']['status'] = 'FAILURE'
@@ -185,45 +215,57 @@ def get_pending_approvals(pipeline_json):
                     table[deployment['environmentForPendingApproval']] = deployment['id']
     return table
 
-def approve_deployment(deployment_id):
-    response = CD4PE_CLIENT.approve_deployment(deployment_id=deployment_id).json()
-    return response
+def request_snow_change(endpoint, deployment_id):
+    headers = {'Content-Type': 'application/json'}
+    json = {}
+    json['commitSHA'] = data['scm']['commit']
+    json['deploymentId'] = deployment_id
+    json['targetBranch'] = 'production'
+    print_json(json)
+    #webhook_response = requests.post(endpoint, headers=headers, json=json)
+    #print(webhook_response)
+
+def trigger_webhook(endpoint, data):
+    headers = {'Content-Type': 'application/json'}
+    webhook_response = requests.post(endpoint, headers=headers, json=data)
+    print(webhook_response)
 
 # Start of code execution
-data = {}
-data['name'] = 'cd4pe-pipeline'
-data['display_name'] = 'cd4pe-pipeline'
-data['build'] = {}
-data['log'] = ""
-data['notes'] = ""
-
 connect_cd4pe(endpoint=endpoint, username=user, password=pwd)
 pipeline = search_latest_pipeline(repo_name=repo, gitCommitId=commitSha)
-pipeline_json = CD4PE_CLIENT.get_pipeline(repo_name=repo, pipeline_id=pipeline['id']).json()
 
-data['url'] = "main/repositories/" + repo + "?pipelineId=" + str(pipeline['id'])
-data['build']['full_url'] = endpoint + "/" + CD4PE_CLIENT.api_ajax.owner + "/repositories/" + repo + "?pipelineId=" + str(pipeline['id']) + "&eventId=" + str(pipeline['eventId'])
-data['build']['number'] = pipeline['eventId']
-data['build']['phase'] = stagename
-data['build']['url'] = "/" + CD4PE_CLIENT.api_ajax.owner + "/repositories/" + repo + "?pipelineId=" + str(pipeline['id']) + "&eventId=" + str(pipeline['eventId'])
-data['scm'] = {}
-data['scm']['url'] = pipeline_json['buildStage']['imageEvent']['repoUrl']
-data['scm']['branch'] = pipeline_json['buildStage']['imageEvent']['branch']
-data['scm']['commit'] = pipeline_json['buildStage']['imageEvent']['commitId']
-data['scm']['changes'] = []
-data['scm']['culprits'] = []
-data['artifacts'] = {}
+if changereq:
+    pending_approvals = get_pending_approvals(repo_name=repo, pipeline_id=pipeline_id)
+    request_snow_change(endpoint='https://ven02941.service-now.com/api/x_radi_rapdev_pupp/change_request', deployment_id=pending_approvals['production'])
+else:
+    data = {}
+    data['name'] = 'cd4pe-pipeline'
+    data['display_name'] = 'cd4pe-pipeline'
+    data['build'] = {}
+    data['build']['events'] = []
+    data['log'] = ""
+    data['notes'] = ""
+    pipeline_json = CD4PE_CLIENT.get_pipeline(repo_name=repo, pipeline_id=pipeline['id']).json()
+    data['url'] = "main/repositories/" + repo + "?pipelineId=" + str(pipeline['id'])
+    data['build']['full_url'] = endpoint + "/" + CD4PE_CLIENT.api_ajax.owner + "/repositories/" + repo + "?pipelineId=" + str(pipeline['id']) + "&eventId=" + str(pipeline['eventId'])
+    data['build']['number'] = pipeline['eventId']
+    data['build']['phase'] = stagename
+    data['build']['url'] = "/" + CD4PE_CLIENT.api_ajax.owner + "/repositories/" + repo + "?pipelineId=" + str(pipeline['id']) + "&eventId=" + str(pipeline['eventId'])
+    data['scm'] = {}
+    data['scm']['url'] = pipeline_json['buildStage']['imageEvent']['repoUrl']
+    data['scm']['branch'] = pipeline_json['buildStage']['imageEvent']['branch']
+    data['scm']['commit'] = pipeline_json['buildStage']['imageEvent']['commitId']
+    data['scm']['changes'] = []
+    data['scm']['culprits'] = []
+    data['artifacts'] = {}
 
-pipeline_report = report_pipeline_stages(pipeline_json=pipeline_json, pipeline_stage=stagename)
-if stagename == 'Impact Analysis':
-    IA = get_IA_from_pipeline(pipeline_json=pipeline_json)
-    if IA['Status'] == "DONE":
-        report = get_impact_analysis_node_report(impact_analysis_id=IA['Id'], max_changes=maxchanges)
-        data['notes'] = report
+    pipeline_report = report_pipeline_stages(pipeline_json=pipeline_json, pipeline_stage=stagename)
+    if stagename == 'Impact Analysis':
+        IA = get_IA_from_pipeline(pipeline_json=pipeline_json)
+        if IA['Status'] == "DONE":
+            report = get_impact_analysis_node_report(impact_analysis_id=IA['Id'], max_changes=maxchanges)
+            data['notes'] = report
 
-print_json(data)
+    print_json(data)
 
-webhook_endpoint = 'https://ven02941.service-now.com/api/x_radi_rapdev_pupp/pipeline_webhook'
-headers = {'Content-Type': 'application/json'}
-webhook_response = requests.post(webhook_endpoint, headers=headers, json=data)
-print(webhook_response)
+    #trigger_webhook(endpoint='https://ven02941.service-now.com/api/x_radi_rapdev_pupp/pipeline_webhook', data=data)
