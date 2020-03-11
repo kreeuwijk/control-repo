3571b4cf3b8774013d603c9c37dfed85223a1104
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-03-11 17:40:56 +0100

    deploy Puppetfile

--- site-modules/deployments/plans/testing.pp
+++ site-modules/deployments/plans/testing.pp
@@ -6,7 +6,7 @@ plan deployments::testing(
   $repo_name = 'control-repo'
   #$events = cd4pe_deployments::list_trigger_events($repo_name)
   #file::write('/root/testoutput.txt', $events)
-  $dir = module_directory('deployments')
-  out::message("${dir}")
-  file::write('/root/testoutput.txt', $dir)
+  $deploysdir = module_directory('deployments')  # => /disk/2813153411337507734/control-repo/site-modules/deployments
+  run_command("bolt puppetfile install --puppetfile ${deploysdir}/Puppetfile --modulepath ${deploysdir}/..", 'localhost')
+  ctrl::sleep(60)
 }
