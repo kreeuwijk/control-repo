9f99e754875a480f29e8610f072124826a855d17
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-03-11 18:56:24 +0100

    correct params

--- site-modules/deployments/plans/testing.pp
+++ site-modules/deployments/plans/testing.pp
@@ -6,7 +6,7 @@ plan deployments::testing(
   $repo_name = 'control-repo'
   $deploysdir = module_directory('deployments')  # => /disk/2813153411337507734/control-repo/site-modules/deployments
   run_task('deployments::prereqs', localhost, 'Ensure prereqs are installed...')
-  run_task('deployments::install_modules', 'localhost', 'deploydir' => $deploysdir, 'Install module content...')
+  run_task('deployments::install_modules', 'localhost', 'Install module content...', 'deploydir' => $deploysdir)
   ctrl::sleep(180)
   #$events = cd4pe_deployments::list_trigger_events($repo_name)
   #file::write('/root/testoutput.txt', $events)
