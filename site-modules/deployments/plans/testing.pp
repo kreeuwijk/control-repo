812fff97fbc5e74816bcedcb2bb99414faccab63
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-03-11 18:54:08 +0100

    add prereqs, correct modulepath

--- site-modules/deployments/plans/testing.pp
+++ site-modules/deployments/plans/testing.pp
@@ -4,10 +4,11 @@ plan deployments::testing(
 ){
   $repo_type = system::env('REPO_TYPE')
   $repo_name = 'control-repo'
+  $deploysdir = module_directory('deployments')  # => /disk/2813153411337507734/control-repo/site-modules/deployments
+  run_task('deployments::prereqs', localhost, 'Ensure prereqs are installed...')
+  run_task('deployments::install_modules', 'localhost', 'deploydir' => $deploysdir, 'Install module content...')
+  ctrl::sleep(180)
   #$events = cd4pe_deployments::list_trigger_events($repo_name)
   #file::write('/root/testoutput.txt', $events)
-  $deploysdir = module_directory('deployments')  # => /disk/2813153411337507734/control-repo/site-modules/deployments
-  #run_command("bolt puppetfile install --puppetfile ${deploysdir}/Puppetfile --modulepath ${deploysdir}/..", 'localhost')
-  run_task('deployments::install_modules', 'localhost', 'deploydir' => $deploysdir)
-  ctrl::sleep(180)
+
 }
