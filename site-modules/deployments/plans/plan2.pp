e98d5d840875235d8330de425e6f091f045fd284
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-03-12 15:18:55 +0100

    test with auth

--- site-modules/deployments/plans/plan2.pp
+++
@@ -1,5 +0,0 @@
-plan deployments::plan2(
-){
-  $result = servicenow_integration::testfunction('hello')
-  file::write('/root/testoutput.txt', $result)
-}
--- site-modules/deployments/plans/testing.pp
+++ site-modules/deployments/plans/testing.pp
@@ -1,14 +1,13 @@
 plan deployments::testing(
-  Optional[Integer] $max_node_failure,
-  Optional[Boolean] $noop = false,
+  String $cd4pe_user,
+  Sensitive $cd4pe_passwd,
 ){
   $repo_type = system::env('REPO_TYPE')
   $repo_name = 'control-repo'
   $deploysdir = module_directory('deployments')  # => /disk/2813153411337507734/control-repo/site-modules/deployments
-  #run_task('deployments::prereqs', localhost, 'Ensure prereqs are installed...')
-  #run_task('deployments::install_modules', 'localhost', 'Install module content...', 'deploydir' => $deploysdir)
   #ctrl::sleep(180)
-  $events = cd4pe_deployments::list_trigger_events($repo_name)
+  $cookie = cd4pe_deployments::get_cookie($cd4pe_user, $cd4pe_pwd.unwrap)
+  $events = cd4pe_deployments::list_trigger_events($repo_name, $cookie)
   file::write('/root/testoutput.txt', "${events}")
 
 }
