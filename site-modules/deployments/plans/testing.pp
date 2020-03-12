67325ba55d4a82336ae69b37555680850c1a3b3b
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-03-12 15:20:12 +0100

    no support for Sensitive yet

--- site-modules/deployments/plans/testing.pp
+++ site-modules/deployments/plans/testing.pp
@@ -1,12 +1,12 @@
 plan deployments::testing(
   String $cd4pe_user,
-  Sensitive $cd4pe_passwd,
+  String $cd4pe_passwd,
 ){
   $repo_type = system::env('REPO_TYPE')
   $repo_name = 'control-repo'
   $deploysdir = module_directory('deployments')  # => /disk/2813153411337507734/control-repo/site-modules/deployments
   #ctrl::sleep(180)
-  $cookie = cd4pe_deployments::get_cookie($cd4pe_user, $cd4pe_pwd.unwrap)
+  $cookie = cd4pe_deployments::get_cookie($cd4pe_user, $cd4pe_pwd)
   $events = cd4pe_deployments::list_trigger_events($repo_name, $cookie)
   file::write('/root/testoutput.txt', "${events}")
 
