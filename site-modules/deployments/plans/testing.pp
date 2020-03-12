5431b65dbcb9fbe34399c005cddd55217699c587
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-03-12 10:20:39 +0100

    test

--- site-modules/deployments/plans/testing.pp
+++ site-modules/deployments/plans/testing.pp
@@ -5,11 +5,10 @@ plan deployments::testing(
   $repo_type = system::env('REPO_TYPE')
   $repo_name = 'control-repo'
   $deploysdir = module_directory('deployments')  # => /disk/2813153411337507734/control-repo/site-modules/deployments
-  run_task('deployments::prereqs', localhost, 'Ensure prereqs are installed...')
-  run_task('deployments::install_modules', 'localhost', 'Install module content...', 'deploydir' => $deploysdir)
+  #run_task('deployments::prereqs', localhost, 'Ensure prereqs are installed...')
+  #run_task('deployments::install_modules', 'localhost', 'Install module content...', 'deploydir' => $deploysdir)
   #ctrl::sleep(180)
-  #$events = cd4pe_deployments::list_trigger_events($repo_name)
-  #file::write('/root/testoutput.txt', $events)
-  run_plan('deployments::plan2')
+  $events = cd4pe_deployments::list_trigger_events($repo_name)
+  file::write('/root/testoutput.txt', $events)
 
 }
