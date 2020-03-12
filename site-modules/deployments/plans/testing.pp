97220a7bf61507bc6af5f5deb19e5fb52b3786e7
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-03-12 17:11:37 +0100

    testing

--- site-modules/deployments/plans/testing.pp
+++ site-modules/deployments/plans/testing.pp
@@ -6,10 +6,9 @@ plan deployments::testing(
   $repo_name = 'control-repo'
   $deploysdir = module_directory('deployments')  # => /disk/2813153411337507734/control-repo/site-modules/deployments
   #ctrl::sleep(180)
-  file::write('/root/test_creds.txt', "${cd4pe_user} | ${cd4pe_passwd}")
   $cookie = cd4pe_deployments::get_cookie($cd4pe_user, $cd4pe_passwd)
   file::write('/root/test_cookie.txt', "${cookie}")
-  #$events = cd4pe_deployments::list_trigger_events($repo_name, $cookie)
-  #file::write('/root/testoutput.txt', "${events}")
+  $events = cd4pe_deployments::list_trigger_events($repo_name, $cookie)
+  file::write('/root/testoutput.txt', "${events}")
 
 }
