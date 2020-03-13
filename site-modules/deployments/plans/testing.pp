b545f0c0c9711cc1870c6b3f0ece6305cc16ba1c
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-03-13 12:35:18 +0100

    test

--- site-modules/deployments/plans/testing.pp
+++ site-modules/deployments/plans/testing.pp
@@ -2,13 +2,16 @@ plan deployments::testing(
   String $cd4pe_user,
   String $cd4pe_passwd,
 ){
+  # Read control repo type from CD4PE environment variable
   $repo_type = system::env('REPO_TYPE')
+  # Hardcode control repo name, as this can't be read from the env vars yet
   $repo_name = 'control-repo'
-  $deploysdir = module_directory('deployments')  # => /disk/2813153411337507734/control-repo/site-modules/deployments
-  #ctrl::sleep(180)
+
+  # Get a cookie for function calls that need it
   $cookie = cd4pe_deployments::get_cookie($cd4pe_user, $cd4pe_passwd)
-  file::write('/root/test_cookie.txt', "${cookie}")
   $events = cd4pe_deployments::list_trigger_events($repo_name, $cookie['result'])
-  file::write('/root/testoutput.txt', "${events}")
+
+  $deploysdir = module_directory('deployments')  # => /disk/2813153411337507734/control-repo/site-modules/deployments
+  file::write('/root/testoutput.txt', "${deploysdir}")
 
 }
