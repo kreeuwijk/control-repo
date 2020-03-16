fe33e08e7f0cc57a87cce57fd970af8f60497113
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-03-16 16:08:21 +0100

    test

--- site-modules/deployments/lib/puppet/functions/deployments/env_test.rb
+++ site-modules/deployments/lib/puppet/functions/deployments/env_test.rb
@@ -3,6 +3,10 @@ Puppet::Functions.create_function(:'deployments::env_test') do
   end
 
   def env_test
-    ENV['COMMIT']
+    result_hash = {}
+    ENV.each do |env_var|
+      result_hash = result_hash.merge(env_var)
+    end
+    return result_hash
   end
 end
