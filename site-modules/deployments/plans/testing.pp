9e2fd9839de7a09ab4b13de531a7cc60afb21687
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-03-16 13:58:33 +0100

    test

--- site-modules/deployments/plans/testing.pp
+++ site-modules/deployments/plans/testing.pp
@@ -4,15 +4,18 @@ plan deployments::testing(
 ){
   # Read control repo type from CD4PE environment variable
   $repo_type = system::env('REPO_TYPE')
+  $commit_sha = system::env('COMMIT')
   # Hardcode control repo name, as this can't be read from the env vars yet
   $repo_name = 'control-repo'
 
   # Get a cookie for function calls that need it
   $cookie = cd4pe_deployments::get_cookie($cd4pe_user, $cd4pe_passwd)
-  $events = cd4pe_deployments::list_trigger_events($repo_name, $cookie['result'])
-
-  $deploysdir = module_directory('deployments')  # => /disk/2813153411337507734/control-repo/site-modules/deployments
-  file::write('/root/testoutput.txt', "${deploysdir}")
-  ctrl::sleep(120)
+  $pipeline_id = deployments::search_pipeline($repo_name, $commit_sha, $cookie)
+  $pipeline = deployments::get_pipeline($repo_type, $repo_name, $pipeline_id, $cookie)
+  file::write('/root/testoutput.txt', "${pipeline}")
 
 }
+
+#$deploysdir = module_directory('deployments')  # => /disk/2813153411337507734/control-repo/site-modules/deployments
+#file::write('/root/testoutput.txt', "${deploysdir}")
+#ctrl::sleep(120)
