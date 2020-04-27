926f722c98ecc9b45dcc92aae86f3f98cb73f430
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-04-27 19:53:13 +0200

    remove need for cookies, repo name

--- site-modules/deployments/plans/servicenow_integration.pp
+++ site-modules/deployments/plans/servicenow_integration.pp
@@ -1,7 +1,4 @@
 plan deployments::servicenow_integration(
-  String $cd4pe_user,
-  String $cd4pe_passwd,
-  String $repo_name,
   String $snow_endpoint,
   Optional[Integer] $max_changes_per_node = 10,
   Optional[Integer] $ia_stage = undef,
@@ -12,13 +9,13 @@ plan deployments::servicenow_integration(
   # Read relevant CD4PE environment variables
   $repo_type         = system::env('REPO_TYPE')
   $commit_sha        = system::env('COMMIT')
-  #$control_repo_name = system::env('CONTROL_REPO_NAME')
-  #$module_name       = system::env('MODULE_NAME')
+  $control_repo_name = system::env('CONTROL_REPO_NAME')
+  $module_name       = system::env('MODULE_NAME')
 
-  #$repo_name = $repo_type ? {
-  #  'CONTROL_REPO' => $control_repo_name,
-  #  'MODULE' => $module_name
-  #}
+  $repo_name = $repo_type ? {
+    'CONTROL_REPO' => $control_repo_name,
+    'MODULE' => $module_name
+  }
 
   # Find out which stage we should report on first
   if $ia_stage == undef {
@@ -27,12 +24,8 @@ plan deployments::servicenow_integration(
     $stage_num = "${ia_stage}"
   }
 
-  # Get a cookie for function calls that need it
-  $cookie_result = cd4pe_deployments::get_cookie($cd4pe_user, $cd4pe_passwd)
-  $cookie = deployments::eval_result($cookie_result)
-
   # Find the pipeline ID for the commit SHA
-  $pipeline_search_result = cd4pe_deployments::search_pipeline($repo_name, $commit_sha, $cookie)
+  $pipeline_search_result = cd4pe_deployments::search_pipeline($repo_name, $commit_sha)
   $pipeline_search_hash = deployments::eval_result($pipeline_search_result)
   $pipeline_id = $pipeline_search_hash['id']
 
@@ -41,7 +34,7 @@ plan deployments::servicenow_integration(
     # Wait 15 seconds for each loop
     ctrl::sleep(1)
     # Get the current pipeline stage status (temporary variables that don't exist outside this loop)
-    $pipeline_result = cd4pe_deployments::get_pipeline_trigger_event($repo_name, $pipeline_id, $commit_sha, $cookie)
+    $pipeline_result = cd4pe_deployments::get_pipeline_trigger_event($repo_name, $pipeline_id, $commit_sha)
     $pipeline = deployments::eval_result($pipeline_result)
     # Check if items in the pipeline stage are done
     deployments::pipeline_stage_done($pipeline['eventsByStage'][$stage_num])
@@ -50,7 +43,7 @@ plan deployments::servicenow_integration(
     fail_plan('Timeout waiting for pipeline stage to finish!', 'timeout_error')
   }
   # Now that the relevant jobs in the pipeline stage have completed, generate the final pipeline variables
-  $pipeline_result = cd4pe_deployments::get_pipeline_trigger_event($repo_name, $pipeline_id, $commit_sha, $cookie)
+  $pipeline_result = cd4pe_deployments::get_pipeline_trigger_event($repo_name, $pipeline_id, $commit_sha)
   $pipeline = deployments::eval_result($pipeline_result)
 
   # Gather pipeline stage reporting
@@ -62,13 +55,13 @@ plan deployments::servicenow_integration(
   if $ia_events.length > 0 {
     # Get the Impact Analysis information
     $impact_analysis_id = $ia_events[0]['eventNumber']
-    $impact_analysis_result = cd4pe_deployments::get_impact_analysis($impact_analysis_id, $cookie)
+    $impact_analysis_result = cd4pe_deployments::get_impact_analysis($impact_analysis_id)
     $impact_analysis = deployments::eval_result($impact_analysis_result)
     $ia_report = deployments::report_impact_analysis($impact_analysis)
 
     # Generate the detailed Impact Analysis report
     $ia_envs_report = $ia_report['results'].map |$ia_env_report| {
-      $impacted_nodes_result = cd4pe_deployments::search_impacted_nodes($ia_env_report['IA_resultId'], $cookie)
+      $impacted_nodes_result = cd4pe_deployments::search_impacted_nodes($ia_env_report['IA_resultId'])
       $impacted_nodes = deployments::eval_result($impacted_nodes_result)
       deployments::report_impacted_nodes($ia_env_report, $impacted_nodes, $max_changes_per_node)
     }
