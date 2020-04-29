ead5c1b7ab9a3b754007c43971e56d816223152c
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-04-29 11:32:34 +0200

    cleanup

--- site-modules/deployments/lib/puppet/functions/print_json.rb
+++
@@ -1,6 +0,0 @@
-require 'json'
-Puppet::Functions.create_function(:print_json) do
-  def print_json(data)
-    puts JSON.pretty_generate(data)
-  end
-end
--- site-modules/deployments/tasks/prereqs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if !(dpkg -l git); then
-  apt-get update && apt-get install -y git
-fi
