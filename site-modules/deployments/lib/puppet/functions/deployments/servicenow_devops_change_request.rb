f75e37609cf3023c5633a910291284f68bcc43fb
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-05-14 12:14:39 +0200

    correctly handle promote stage number

--- site-modules/deployments/lib/puppet/functions/deployments/servicenow_devops_change_request.rb
+++ site-modules/deployments/lib/puppet/functions/deployments/servicenow_devops_change_request.rb
@@ -6,10 +6,10 @@ Puppet::Functions.create_function(:'deployments::servicenow_devops_change_reques
   dispatch :servicenow_devops_change_request do
     required_param 'String', :endpoint
     required_param 'Hash', :report
-    required_param 'String', :current_stage
+    required_param 'Integer', :promote_to_stage
   end
 
-  def servicenow_devops_change_request(endpoint, report, current_stage)
+  def servicenow_devops_change_request(endpoint, report, promote_to_stage)
     changereq = {}
     changereq['commitSHA']       = report['scm']['commit']
     changereq['deploymentId']    = report['build']['number']
@@ -17,7 +17,7 @@ Puppet::Functions.create_function(:'deployments::servicenow_devops_change_reques
     changereq['workspace']       = report['build']['owner']
     changereq['repoName']        = report['build']['repo_name']
     changereq['repoType']        = report['build']['repo_type']
-    changereq['promoteToStage']  = current_stage.to_i + 1
+    changereq['promoteToStage']  = promote_to_stage
     changereq['scm_branch']      = report['scm']['branch']
     bln_ia_safe_verdict = true
     report['notes'].each do |ia|
